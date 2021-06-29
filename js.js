30683f8488a0dc751011ab80ac2c510d364b3fee
BananaKun <yanxun951224@gmail.com>
2021-06-29 15:44:12 +0100

    Ex 7 - Ex 9

--- js.js
+++ js.js
@@ -1,19 +1,30 @@
-// This additional exercises, from 7 to 12, use JavaScript as well
-
-// EX7.: Write a function to change the h1 text
-
-// EX8.: Write a function to change the page background color
-
-// EX9.: Write a function to change the footer address with a fake one
 
 // EX10.: Write a function to add a CSS class to every Amazon link
 
 // EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
 
 // EX12: Write a function to color the price of each product in a different one every time it's invoked
-lang="text/javascript";
-
 let changeH1Text = function(){
-    document.getElementById("h1").innerHTML = "yourTextHere";
-    alert("hi");
+    document.getElementsByTagName("h1")[0].innerHTML = "Not So Smartphone Shop";
+}
+
+let changeBackgroundColor = function(){
+    document.body.style.backgroundColor = "lightgreen";
+}
+
+let changeFooterAddress = function(){
+    document.getElementById("address").innerHTML = "<strong>Address: Mars</strong>";
+}
+
+let addClass = function(){
+    let table = document.getElementById('myTable');
+    let rowLength = table.rows.length;
+    for (i = 0; i < rowLength; i++){
+
+        let oCells = oTable.rows.item(i).cells;
+        let cellLength = oCells.length;
+        for(let j = 0; j < cellLength; j++){
+            
+        }
+     }
 }
